d4121b8ea7d75e5111d150f7eb5b9ca1d05e61a7
Larko_0 <larionov1001@gmail.com>
2021-03-20 14:38:14 +0200

    Added description

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def human_say_smth(name, text):
     """
     Говорит что-то от имени кого-то
     """
+    print('-' * 30)
     print(f"{name}: '{text}'")
+    print('-' * 30)
 
 
 human_say_smth('Bob', 'I love pizza')
