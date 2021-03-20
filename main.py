1a3a8b6f2988de92e3b6c620c5bd99a8b40c9336
Larko_0 <larionov1001@gmail.com>
2021-03-20 14:42:49 +0200

    functional -> OOP style

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-def human_say_smth(name, text):
-    """
-    Говорит что-то от имени кого-то
-    """
-    print('-' * 30)
-    print(f"{name}: '{text}'")
-    print('-' * 30)
+class Human:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def say_smth(self, text):
+        """
+            Говорит что-то от имени кого-то
+        """
+        print('-' * 30)
+        print(f"{self.name}: '{text}'")
+        print('-' * 30)
 
 
-human_say_smth('Bob', 'I love pizza')
+h = Human('Bob', 20)
+h.say_smth('I love pizza')
